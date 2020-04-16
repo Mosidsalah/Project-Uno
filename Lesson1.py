17eafde0d1932c34c4fe9bb9ad99e554bd15f891
Siddhant Agarwal <siddhantagarwal@Siddhants-MacBook-Air.local>
2020-04-16 14:47:23 +0530

    First Commit

---
+++ Lesson1.py
@@ -0,0 +1,6 @@
+import random
+print ('Yo')
+bands = ['Green Day','Arctic Monkeys','We the Kings','Post Malone','Death Cab for Cutie']
+print(bands[0])
+band_choice = random.choice(bands)
+print('Randomly selected band is:',band_choice)
